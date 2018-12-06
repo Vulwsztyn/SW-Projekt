ad974a4fed3b68d3e80e0274be29e3ee1cece32e
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2018-12-06 19:14:00 +0100

    Polskie znaki

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ from LampkiDict import slownikLampek
 from zegar import ZegarBinarny
 import random
 
-
 # LED strip configuration:
 LED_COUNT = 50  # Number of LED pixels.
 # LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_PIN = 10  # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 30  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -27,30 +26,33 @@ def clear(strip):
         strip.setPixelColor(i, color)
     strip.show()
 
+
 def zapalPojedyncze(strip, i):
     strip.setPixelColor(i, losowyKolor())
     strip.show()
-    strip.setPixelColor(i, Color(0,0,0))
+    strip.setPixelColor(i, Color(0, 0, 0))
 
 
-def wyswietlLitere(strip,litera):
+def wyswietlLitere(strip, litera):
     czas = 1
     if litera == " ":
-        time.sleep(czas*0.5)
+        time.sleep(czas * 0.5)
         return
     if litera == ".":
         time.sleep(czas * 2)
         return
+
+    if ord(unicode(litera)) > 190:
+        litera = ord(unicode(litera))
     if litera in list(slownikLampek.keys()):
-        zapalPojedyncze(strip,slownikLampek[litera])
+        zapalPojedyncze(strip, slownikLampek[litera])
         time.sleep(czas)
         strip.show()
 
 
-def wyswietlTekst(strip,text):
+def wyswietlTekst(strip, text):
     for i in text:
-        wyswietlLitere(strip,i)
-
+        wyswietlLitere(strip, i)
     strip.show()
 
 
@@ -58,7 +60,9 @@ def getFromWebsite():
     response = requests.get('http://amostowski.pl/sw/get.php')
     # print(response.status_code)
     # print(response.content)
-    content = str(response.content)
+    content = response.content.decode('utf-8')
+    #    numer = content[2:content.find(' ')]
+    #    tekst = content[content.find(' ') + 1:len(content) - 5]
     numer = content[0:content.find(' ')]
     tekst = content[content.find(' ') + 1:len(content) - 1]
     return int(numer), tekst[0:100]
@@ -80,9 +84,9 @@ if __name__ == "__main__":
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     strip.begin()
-    while 2>1:
+    while 2 > 1:
         clear(strip)
-        numer,tresc = getFromWebsite()
+        numer, tresc = getFromWebsite()
         if numer == 0:
             ZegarBinarny(strip)
             time.sleep(1)
